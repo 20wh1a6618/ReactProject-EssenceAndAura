94874beaa78fe54b44e75c131710db2f3348f164
unknown <20wh1a6618@bvrithyderabad.edu.in>
2024-12-26 19:44:51 +0530

    first commit

--- src/store.js
+++ src/store.js
@@ -1,35 +1,35 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// Product slice
 const productSlice = createSlice({
     name: 'products',
     initialState: {
         women: [
-            { brand: "Chanel", name: "Coco Mademoiselle", price: 150, image: "public/images/Chanel - Bleu de Chanel.jpg"},
-            { brand: "Dior", name: "J'adore", price: 135, image: "public/images/Dior - J'adore.jpg" },
-            { brand: "Yves Saint Laurent", name: "Black Opium", price: 130, image: "public/images/Yves Saint Laurent - Black Opium.jpg" },
-            { brand: "Tom Ford", name: "Black Orchid", price: 180, image: "public/images/Tom Ford - Black Orchid.jpg" },
-            { brand: "Lancôme", name: "La Vie Est Belle", price: 125, image: "public/images/Lancôme - La Vie Est Belle.jpg" },
-            { brand: "Gucci", name: "Bloom", price: 120, image: "public/images/Gucci - Bloom.jpg" },
-            { brand: "Viktor & Rolf", name: "Flowerbomb", price: 165, image: "public/images/Viktor & Rolf - Flowerbomb.jpg" },
-            { brand: "Dolce & Gabbana", name: "Light Blue", price: 100, image: "public/images/Dolce & Gabbana - Light Blue.jpg" },
-            { brand: "Jo Malone", name: "English Pear & Freesia", price: 144, image: "public/images/Jo Malone - English Pear & Freesia.jpg" },
-            { brand: "Hermès", name: "Twilly d'Hermès", price: 145, image: "public/images/Hermès - Twilly d'Hermès.jpg" }
+            { brand: "Chanel", name: "Coco Mademoiselle", price: 150, image: "/Images/ChanelCocoMademoiselleEDPWomen.jpg" },
+            { brand: "Dior", name: "J'adore", price: 135, image: "/Images/DiorJ'adore.jpg" },
+            { brand: "Yves Saint Laurent", name: "Black Opium", price: 130, image: "/Images/YvesSaintLaurentBlackOpium.jpg" },
+            { brand: "Tom Ford", name: "Black Orchid", price: 180, image: "/Images/TomFordBlackOrchid.jpg" },
+            { brand: "Lancôme", name: "La Vie Est Belle", price: 125, image: "/Images/LancômeLaVieEstBelle.jpg" },
+            { brand: "Gucci", name: "Bloom", price: 120, image: "/Images/GucciBloom.jpg" },
+            { brand: "Viktor & Rolf", name: "Flowerbomb", price: 165, image: "/Images/ViktorRolfFlowerbomb.jpg" },
+            { brand: "Dolce & Gabbana", name: "Light Blue", price: 100, image: "/Images/DolceGabbanaLightBlue.jpgv" },
+            { brand: "Jo Malone", name: "English Pear & Freesia", price: 144, image: "/Images/JoMaloneEnglishPearFreesia.jpg" },
+            { brand: "Hermès", name: "Twilly d'Hermès", price: 145, image: "/Images/HermèsTwillyd'Hermès.jpg" }
         ],
         men: [
-            { brand: "Creed", name: "Aventus", price: 445, image: "public/images/Creed - Aventus.jpg" },
-            { brand: "Dior", name: "Sauvage Eau de Parfum", price: 125, image: "public/images/Dior - Sauvage Eau de Parfum.jpg" },
-            { brand: "Tom Ford", name: "Oud Wood", price: 270, image: "public/images/Tom Ford - Oud Wood.jpg" },
-            { brand: "Chanel", name: "Bleu de Chanel", price: 150, image: "public/images/Chanel - Bleu de Chanel.jpg" },
-            { brand: "Giorgio Armani", name: "Acqua di Gio Profumo", price: 135, image: "public/images/Giorgio Armani - Acqua di Gio Profumo.jpg" },
-            { brand: "Yves Saint Laurent", name: "La Nuit de l'Homme", price: 95, image: "public/images/Yves Saint Laurent - La Nuit de l'Homme.jpg" },
-            { brand: "Hermès", name: "Terre d’Hermès", price: 135, image: "public/images/Hermès - Terre d’Hermès.jpg" },
-            { brand: "Versace", name: "Eros", price: 105, image: "public/images/Versace - Eros.jpg" },
-            { brand: "Paco Rabanne", name: "1 Million", price: 95, image: "public/images/Paco Rabanne - 1 Million.jpg" },
-            { brand: "Jean Paul Gaultier", name: "Le Male", price: 95, image: "public/images/Jean Paul Gaultier - Le Male.jpg" }
+            { brand: "Creed", name: "Aventus", price: 445, image: "/Images/CreedAventus.jpg" },
+            { brand: "Dior", name: "Sauvage Eau de Parfum", price: 125, image: "/Images/DiorSauvageEaudeParfum.jpg" },
+            { brand: "Tom Ford", name: "Oud Wood", price: 270, image: "/Images/TomFordOudWood.jpg" },
+            { brand: "Chanel", name: "Bleu de Chanel", price: 150, image: "/Images/ChanelBleudeChanel.jpg" },
+            { brand: "Giorgio Armani", name: "Acqua di Gio Profumo", price: 135, image: "/Images/GiorgioArmaniAcquadiGioProfumo.jpg" },
+            { brand: "Yves Saint Laurent", name: "La Nuit de l'Homme", price: 95, image: "/Images/YvesSaintLaurentLaNuitdel'Homme.jpg" },
+            { brand: "Hermès", name: "Terre d’Hermès", price: 135, image: "/Images/HermèsTerred’Hermès.jpg" },
+            { brand: "Versace", name: "Eros", price: 105, image: "/Images/VersaceEros.jpg" },
+            { brand: "Paco Rabanne", name: "1 Million", price: 95, image: "/Images/PacoRabanne1Million.jpg" },
+            { brand: "Jean Paul Gaultier", name: "Le Male", price: 95, image: "/Images/JeanPaulGaultierLeMale.jpg" }
         ]
     }
 });
+
 
 // Cart slice
 const cartSlice = createSlice({
